8ebb5becae947ce2d38753c2ee6b1b16933986f7
Beyondthere <mattkrum14@gmail.com>
2025-11-24 15:01:51 -0500

    Create pusle1.c added some possible code to fix issues

---
+++ C/test/pusle1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#include "rp.h"
+#include "rp_sweep.h"
+
+using namespace rp_sweep_api;
+
+int main(int argc, char **argv){
+
+
+if(rp_Init() != RP_OK){
+	fprintf(stderr, "Rp api init failed!\n");
+}
+
+if(rp_SWInit() != RP_OK){
+  fprintf(stderr, "Sweep init failied!\n");
+  return  -1;
+}
+
+float start_freq = 100000; //in hertz
+float end_freq = 300000; //in hertz
+float sweep_time = 100000; //in micro seconds
+
+rp_GenReset();
+
+rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
+
+rp_GenSweepStartFreq(RP_CH_1, start_freq);
+rp_GenSweepEndFreq(RP_CH_1, end_freq);
+rp_SWSetTime(RP_CH_1, sweep_time);
+
+
+rp_SWSetMode(RP_CH_1, RP_GEN_SWEEP_MODE_LINEAR);
+
+
+rp_GenAmp(RP_CH_1, .3);
+rp_GenOutEnable(RP_CH_1);
+
+
+usleep(sweep_time + 20000); //sleep in micro seconds added margin jsut in case it turns off before
+rp_GenOutDisable(RP_CH_1);
+
+rp_Release();
+return 0;
+
+}
